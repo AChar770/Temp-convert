c46bc17aca5dfcc1742e379bcde56b32b219f88a
AChar770 <amandatcharles@yahoo.com>
2025-12-15 01:55:23 -0500

    answered 3 & 4

--- README.md
+++ README.md
@@ -5,12 +5,9 @@ Let's build an interactive web application to help users convert a temperature i
 ## Instructions
 
 1. Create and connect an `index.js` file to `index.html`. CHECK!
+2. Write a function named `convertToCelsius` which takes a temperature in Fahrenheit and returns the equivalent temperature in Celsius.'
 
 
-
-
-
-2. Write a function named `convertToCelsius` which takes a temperature in Fahrenheit and returns the equivalent temperature in Celsius.
 3. Write a function named `describeTemperature` which takes a temperature in Fahrenheit and returns a description according to this table:
 
    | Temperature (°C) | Description |
--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 function convertToCelsius(Fahrenheit) {
-  return Fahrenheit;
+  return (Fahrenheit - 32) * (5 / 9);
 }
+
+function describeTemperature(f) {
+const celsius = convertToCelsius(f)
+console.log ("Celsius" + celsius)
+
+ if (celsius < 0) 
+    return "very cold"; 
+
+   else if (celsius < 20) {
+    return "cold";
+  } 
+  
+  else if (celsius < 30) {
+    return "warm";
+  } 
+  
+  else if (celsius < 40) {
+    return "hot";
+  } 
+  
+  else {
+    return "very hot";}}
+
+
+const f = prompt("Put in Fahrenheit tempurature for conversion");
+const description = describeTemperature(f);
+alert("This tempurature feels" + description);
